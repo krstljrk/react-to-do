1d9a6722042c855929b6385f9478c3185551147f
krstjrk <kristel.juurik@gmail.com>
2021-08-21 14:50:51 +0300

    changes made to use configureStore from toolkit

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,7 +1,16 @@
-import { createStore } from 'redux';
+/*import { createStore } from 'redux';
 import toDosReducer from './reducers';
 
 
 const store = createStore(toDosReducer);
 
 export default store;
+*/
+
+import { configureStore } from "@reduxjs/toolkit";
+
+export default configureStore({
+    reducer : {
+        
+    }
+})
