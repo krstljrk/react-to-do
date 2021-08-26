5ad502c3f781222aca43af9e64bc35fb6e7c1c1b
krstjrk <kristel.juurik@gmail.com>
2021-08-26 20:55:49 +0300

    created store using configureStore

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+
+const store = configureStore({
+  reducer: {
+    
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
